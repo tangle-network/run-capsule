66be1b40635320038319a4a7d2ea52d312c3c433
tangletools <hello@tangle.tools>
2026-09-25 14:58:25 -0600

    refactor: route narration through agent-integrations

--- src/audio.ts
+++ src/audio.ts
@@ -16,6 +16,7 @@ import * as fs from 'node:fs'
 import * as os from 'node:os'
 import * as path from 'node:path'
 import { promisify } from 'node:util'
+import { openaiConnector, type ResolvedDataSource } from '@tangle-network/agent-integrations'
 
 const execFileAsync = promisify(execFile)
 
